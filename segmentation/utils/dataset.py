2b80346a01fde28ff5aaf851182bda140c50c01b
Ankith Boggaram <Ankithboggaram@users.noreply.github.com>
2025-02-23 12:28:14 -0700

    Migrate image preprocessing to utils/preprocessing.py

--- segmentation/utils/dataset.py
+++ segmentation/utils/dataset.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from torch.utils.data import Dataset
 import torch
+from utils.preprocessing import *
 
 class SegmentationDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transforms=None):
@@ -20,22 +21,12 @@ class SegmentationDataset(Dataset):
         image_path = os.path.join(self.image_dir, image_id)
         mask_path = os.path.join(self.mask_dir, image_id)
         
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-        
-        # Normalizing the image and mask (I'm not sure what datatype we'll end up using, so I've left it as float32)
-        image = image.astype(np.float32) / 255.0
-        mask = mask.astype(np.float32) / 255.0
+        image = load_image(image_path)
+        mask = load_mask(mask_path)
+
+        image = preprocess_image(image, transforms=self.transforms)
+        mask = preprocess_mask(mask)
         
         data_item = {'image': image, 'mask': mask}
-        
-        # In case we want to augment data etc.
-        if self.transforms:
-            data_item = self.transforms(data_item)
-        
-        # Converting to torch tensors
-        data_item['image'] = torch.from_numpy(data_item['image'].transpose(2, 0, 1)) # This transposes the image to (C, H, W) format, and then converts it to a tensor
-        data_item['mask'] = torch.from_numpy(data_item['mask'][None, ...]) # This transposes the mask to (1, H, W) a 1-channel image, and then into a tensor (Needed help for this)
         
         return data_item
