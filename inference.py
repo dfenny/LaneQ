f148ff1277b0877cd706859112383124dd1b4696
Ankith Boggaram <Ankithboggaram@users.noreply.github.com>
2025-04-01 20:03:04 -0600

    Improve inference script + revert to old CNN architecture

--- inference.py
+++ inference.py
@@ -1,115 +1,145 @@
 # Importing all the necessary funtions from segmentation and regression
+import os
 import cv2
 import json
 import torch
 import numpy as np
+import argparse
 from torchvision import transforms
 
+from segmentation.utils.preprocessing import load_image
 from segmentation.inference import load_saved_model as load_saved_segmentation_model, pred_segmentation_mask
 from regression.inference import load_saved_model as load_saved_regression_model, pred_degradation_value
-from segmentation.utils.preprocessing import load_image, get_img_transform, load_mask, apply_img_preprocessing
 
 from degradation_calculation.calculate_degradation import generate_individual_segments_and_dict
 
+
+def run_inference(input_image: str, 
+                  output_dir: str, 
+                  seg_checkpoint: str = "segmentation/experiment_results/final_model.pth", 
+                  reg_checkpoint: str = "regression/experiment_results/checkpoints/cnn_sppf_final_2025-03-31_21-30-50.pth",
+                  mild_threshold: float = 0.33, 
+                  moderate_threshold: float = 0.66) -> dict:
+    """
+    Runs inference for an input image and stores the results in specified directory.
     
-input_image_path = "D:/Ankith B V/Documents/UofU/Datasets/BDD100k/cs6945share/retro_project/bdd100k/images/test/cabc9045-b3349548.jpg"
- 
-# ==============================================
-# Threshold values for degradation
-
-mild_degradation_threshold = 0.3
-moderate_degradation_threshold = 0.6
-
-# ==============================================
-
-# Transforms
-img_transform = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.Resize((720, 1280)),   # ensure resize same is used for mask by setting preprocess_config
-    transforms.ToTensor()
-])
-
-# Device
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-# Segmentation model
-seg_saved_weight_path = "segmentation/experiment_results/douglas/experiment_results/checkpoints/unet_checkpoint_epoch_4.pth"
-segmentation_model_name = "unet"
-segmentation_model_config = {'in_channels': 3, 'out_channels': 1}
-
-# Regression model    
-reg_saved_weight_path = "regression/experiment_results/checkpoints/cnn_sppf_final_2025-03-31_21-30-50.pth"
-regression_model_name = "cnn_sppf"
-regression_model_config = {'in_channels': 3, 'out_dim': 1}
-
-# Load the saved models
-segmentation_model = load_saved_segmentation_model(model_name=segmentation_model_name, saved_weight_path=seg_saved_weight_path, **segmentation_model_config)
-regression_model = load_saved_regression_model(model_name=regression_model_name, saved_weight_path=reg_saved_weight_path, **regression_model_config)
-
-# Now I'll be running inference of segmetnation model and storing it's results in a JSON file for each input image
-
-# Loading image
-input_img = load_image(input_image_path)
-
-# Predicting the segmentation mask
-predicted_mask = pred_segmentation_mask(model=segmentation_model, test_img=input_img, img_transform=img_transform, add_batch_dim=True, device=DEVICE)
-predicted_mask = np.squeeze(predicted_mask)
-predicted_mask = predicted_mask.astype(np.uint8)
-# print(predicted_mask.shape)
-
-# Sample segmentation mask
-# sample_segmentation_mask_path = "damage_ratio_calc_data/damage_ratio_masks/SegmentationClass/183_jpg.rf.c3a79aad316f75fe23536c3bbbd6da51.png"
-# predicted_mask_0 = load_mask(sample_segmentation_mask_path)
-# predicted_mask_0 = cv2.imread(sample_segmentation_mask_path, cv2.IMREAD_GRAYSCALE)
-# print(predicted_mask_0.shape)
-
-# Convert the detctions into individual segments and store information in a JSON file
-filename = input_image_path.split("/")[-1]
-print(filename)
-
-annotations_dict = generate_individual_segments_and_dict(img=input_img, mask=predicted_mask, filename=filename)
-# On second thought, do we even need to store the individual segments in a separate folder? We can just store the segment coords and dynamically generate the segments during regression
-
-print(annotations_dict)
-
-# Store the segment information in a JSON file for the input image (Converting this to a JSON file after the regression model inference makes more sense)
-# with open(f"damage_ratio_calc_data/{"".join(filename.split(".")[0:-1])}.json", "w") as f:
-#     json.dump(annotations_dict, f)
-
-# Next I'll be running inference of regression model on these individual segments and storing it's results in the same JSON file of the input image
-for segment in annotations_dict["annotations"]:
+    Args:   
+        input_image (str): Path to the input image.
+        output_dir (str): Directory to save output images and JSON files.
+        seg_checkpoint (str): Path to the segmentation model checkpoint.
+        reg_checkpoint (str): Path to the regression model checkpoint.
+        mild_threshold (float): Threshold for mild degradation.
+        moderate_threshold (float): Threshold for moderate degradation.
     
-    # TODO: I have a doubt here. Sicne we're only using the coordinates of the bounding box to crop the segment, we might crop in nearby lane lines too. Does anyone know what we can do here?
-    x, y, w, h = segment["bounding_box"]  # Extract bounding box
-    segment_img = input_img[y:y+h, x:x+w]
+    Returns:
+        dict: Dictionary containing paths to output files.
+    """
 
-    # Predicting the degradation value
-    degradation_value = pred_degradation_value(model=regression_model, test_img=segment_img, img_transform=None, add_batch_dim=True, device=DEVICE)
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Transforms
+    img_transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((720, 1280)),
+        transforms.ToTensor()
+    ])
+    
+    # Device
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    # Model configs
+    segmentation_model_config = {'in_channels': 3, 'out_channels': 1}
+    regression_model_config = {'in_channels': 3, 'out_dim': 1}
 
-    # Setting the degradation value to the segment predicted value
-    segment["degradation"] = degradation_value
-    # segment["degradation"] = -1
+    # Load models
+    segmentation_model = load_saved_segmentation_model(model_name="unet", saved_weight_path=seg_checkpoint, **segmentation_model_config)
+    regression_model = load_saved_regression_model(model_name="cnn_sppf", saved_weight_path=reg_checkpoint, **regression_model_config)
+    
+    # Load image
+    input_img = load_image(input_image)
+    
+    # Predict segmentation mask
+    predicted_mask = pred_segmentation_mask(model=segmentation_model, test_img=input_img, img_transform=img_transform, add_batch_dim=True, device=DEVICE)
+    predicted_mask = np.squeeze(predicted_mask).astype(np.uint8) * 255
+    
+    filename = os.path.basename(input_image)
+    filename_without_ext = os.path.splitext(filename)[0]
+    
+    mask_path = os.path.join(output_dir, f"{filename_without_ext}_mask.jpg")
+    cv2.imwrite(mask_path, predicted_mask)
+    
+    # Generate segment annotations
+    annotations_dict = generate_individual_segments_and_dict(img=input_img, mask=predicted_mask, filename=filename)
+    
+    # Run regression model on each segment
+    for segment in annotations_dict["annotations"]:
+        x, y, w, h = segment["bounding_box"]
+        segment_img = input_img[y:y+h, x:x+w]
+        
+        # Do not pass small segments into the regression model
+        if segment_img.shape[0] < 10 or segment_img.shape[1] < 10:
+            continue
+        
+        degradation_value = pred_degradation_value(model=regression_model, test_img=segment_img, img_transform=None, add_batch_dim=True, device=DEVICE)
+        segment["degradation"] = degradation_value
+    
+    # Save JSON file
+    json_path = os.path.join(output_dir, f"{filename_without_ext}.json")
+    with open(json_path, "w") as f:
+        json.dump(annotations_dict, f)
+    
+    # Annotate and save image
+    input_img_annotated = cv2.imread(input_image)
+    input_img_annotated = cv2.resize(input_img_annotated, (1280, 720))
+    
+    # Plot stuff
+    for segment in annotations_dict["annotations"]:
+        x, y, w, h = segment["bounding_box"]
+        degradation = segment["degradation"]
+        
+        if degradation < mild_threshold and degradation > 0:
+            cv2.rectangle(input_img_annotated, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(input_img_annotated, f"{degradation:.2f}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        elif mild_threshold <= degradation < moderate_threshold:
+            cv2.rectangle(input_img_annotated, (x, y), (x+w, y+h), (0, 255, 255), 2)
+            cv2.putText(input_img_annotated, f"{degradation:.2f}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+        elif degradation > mild_threshold:
+            cv2.rectangle(input_img_annotated, (x, y), (x+w, y+h), (0, 0, 255), 2)
+            cv2.putText(input_img_annotated, f"{degradation:.2f}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        
+        
+    
+    annotated_img_path = os.path.join(output_dir, f"{filename_without_ext}_annotated.jpg")
+    cv2.imwrite(annotated_img_path, input_img_annotated)
+    
+    return {
+        "mask_path": mask_path,
+        "json_path": json_path,
+        "annotated_img_path": annotated_img_path
+    }
 
+def main():
+    parser = argparse.ArgumentParser(description="Run inference on an image.")
+    parser.add_argument("input_image", type=str, help="Path to the input image.")
+    parser.add_argument("output_dir", type=str, help="Directory to save output files.")
+    parser.add_argument("--seg_checkpoint", type=str, default="segmentation/experiment_results/final_model.pth", help="Path to segmentation model weights.")
+    parser.add_argument("--reg_checkpoint", type=str, default="regression/experiment_results/checkpoints/cnn_sppf_final_2025-03-31_21-30-50.pth", help="Path to regression model weights.")
+    parser.add_argument("--mild_threshold", default=0.33, type=float, help="Threshold for mild degradation.")
+    parser.add_argument("--moderate_threshold", default=0.66, type=float, help="Threshold for moderate degradation.")
+    
+    args = parser.parse_args()
+    
+    results = run_inference(
+        output_dir=args.output_dir,
+        input_image=args.input_image,
+        mild_threshold=args.mild_threshold,
+        moderate_threshold=args.moderate_threshold,
+        seg_checkpoint=args.seg_checkpoint,
+        reg_checkpoint=args.reg_checkpoint
+    )
 
-# Store all the information in a JSON file of the input image
-filename_without_ext = "".join(filename.split(".")[0:-1])
-with open(f"damage_ratio_calc_data/{filename_without_ext}.json", "w") as f:
-    json.dump(annotations_dict, f)
+    for k, v in results.items():
+        print(f"{k} : {v}")
 
-# Read the JSON file and write down bounding boxes and degradation values on the image
-with open(f"damage_ratio_calc_data/{filename_without_ext}.json", "r") as f:
-    annotations_dict = json.load(f)
-
-input_img = cv2.imread(input_image_path)
-for segment in annotations_dict["annotations"]:
-    x, y, w, h = segment["bounding_box"]
-    cv2.rectangle(input_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.putText(input_img, f"{segment['degradation']:.2f}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-    if segment["degradation"] < mild_degradation_threshold:
-        cv2.rectangle(input_img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    elif segment["degradation"] < moderate_degradation_threshold:
-        cv2.rectangle(input_img, (x, y), (x+w, y+h), (0, 255, 255), 2)
-    else:
-        cv2.rectangle(input_img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-cv2.imwrite(f"damage_ratio_calc_data/{filename_without_ext}_annotated_new.jpg", input_img)
+if __name__ == "__main__":
+    main()
